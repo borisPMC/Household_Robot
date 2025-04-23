572132408d5f96eb19b28ef64e70a7c363cdbf39
Farinee <1105167224@qq.com>
2025-04-23 20:39:09 +0800

    12

--- Hand_Control/kinect.py
+++ Hand_Control/kinect.py
@@ -1,7 +1,7 @@
 import time
 
 import cv2
-
+import numpy as np
 import pykinect_azure as pykinect
 from pykinect_azure import K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH, k4a_float2_t
 
@@ -44,10 +44,22 @@ def getdis(s):
     #print(z_p)
     #print(s)
     z = s[z_s+3 : z_p]
-    #print(z)
+    print(z)
     return int(z)
 
-def dismap():
+def dis(x,y,dep_img):
+    print(x,y)
+
+    pixels = k4a_float2_t((x,y))
+    rgb_depth = dep_img[y, x]
+    try:
+        pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR,
+                                                 K4A_CALIBRATION_TYPE_DEPTH)
+        pos3d_depth = str(pos3d_depth)
+        return getdis(pos3d_depth)
+    except:
+        print("dis detect error")
+        return -3
     pass
 
 
@@ -63,9 +75,37 @@ def use_kinect():   #返回彩图和深度图
     if not ret_color or not ret_depth:
         raise Exception('kinect未能捕获图像，请检查连接')
         return
+    b,g,r,a = cv2.split(color_image)
 
+    color_image = np.stack((b,g,r), axis=2)
     return color_image,transformed_depth_image
 
+def dismap(xmin,ymin,xmax,ymax,dep_img):
+    size = (xmax-xmin) *(ymax- ymin)
+    sum = 0
+    cnt = 0
+    dots  = np.zeros(5520,dtype=int)
+    #ef_dots =
+    if size>5000:
+
+        for i in range(1,5000):
+            x = dis(np.random.randint(xmin,xmax), np.random.randint(ymin,ymax),dep_img)
+            if(x != -3):
+                dots[cnt] = x
+                cnt += 1
+    else:
+        for i in range(xmin,xmax):
+            for j in range(ymin,ymax):
+                #print(xmin,ymin,xmax,ymax)
+                x = dis(i,j,dep_img)
+                if (x != -3):
+                    dots[cnt] = x
+                    cnt += 1
+
+    ef_dots = dots[0:cnt-1]
+
+    value = ef_dots.mean()
+    return value
 
 
 if __name__ == "__main__":
@@ -98,14 +138,13 @@ if __name__ == "__main__":
         pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR,
                                                           K4A_CALIBRATION_TYPE_DEPTH)
         #print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
-
         # Overlay body segmentation on depth image
         frame_texted = color_image.copy()
         pos_string = str(pos3d_depth)
         map3D = postrans(pos_string)
 
 
-        cv2.putText(frame_texted, "o distance:"+ postrans(pos_string) +"mm", (pix_x, pix_y),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(255,50,50))
+        cv2.putText(frame_texted, "o distance:"+ map3D[2] +"mm", (pix_x, pix_y),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(255,50,50))
         cv2.imshow('Transformed Color Image', frame_texted  )
 
         time.sleep(0.05)
