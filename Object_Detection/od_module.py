c15c93331261d32b392b421c520d5039ef957a5b
Farinee <1105167224@qq.com>
2025-04-23 20:40:59 +0800

    12

--- Object_Detection/od_module.py
+++ Object_Detection/od_module.py
@@ -10,6 +10,9 @@ from PIL import Image
 from js2py import eval_js
 from ultralytics import YOLO
 from paddleocr import PaddleOCR
+import sys
+sys.path.append("..")
+from Hand_Control import kinect
 
 """
 META DATA SECTION
@@ -112,7 +115,7 @@ def auto_capture_photo(fpath, cam_index=0, quality=0.8):
 
     return fpath
 
-def detect_medicine_exist(fname: str, im_model: YOLO) -> tuple[list[dict], list]:
+def detect_medicine_exist(fname, im_model: YOLO) -> tuple[list[dict], list]:
 
     # Custom Exception for Expected Error (No bottle case)
     class BottleNotFoundException(Exception):
@@ -120,12 +123,13 @@ def detect_medicine_exist(fname: str, im_model: YOLO) -> tuple[list[dict], list]
 
     # Initialise param
     conf = []
-    coord = [dict] # Format for each item: {"x_min": x_min, "y_min": y_min, "x_max": x_max, "y_max": y_max}
+    coord = [] # Format for each item: {"x_min": x_min, "y_min": y_min, "x_max": x_max, "y_max": y_max}
 
     try:
         # Run inference
         results = im_model(fname)
-
+        #print("result:")
+        # print(results)
         # Process results
         if not results or not results[0].boxes:
             raise BottleNotFoundException()
@@ -136,7 +140,7 @@ def detect_medicine_exist(fname: str, im_model: YOLO) -> tuple[list[dict], list]
             x_min, y_min, x_max, y_max = map(int, box.xyxy[0].tolist())
             coord.append({"x_min": x_min, "y_min": y_min, "x_max": x_max, "y_max": y_max})
             conf.append(box.conf.item())
-            # print(f"Bounding box: ({x_min}, {y_min}, {x_max}, {y_max})")
+            print(f"Bounding box: ({x_min}, {y_min}, {x_max}, {y_max})")
             # print(f"Confidence: {conf*100:.1f}%")
     
     except BottleNotFoundException:
@@ -153,6 +157,8 @@ def detect_with_ocr(ocr_model: PaddleOCR, image, coord_list: list[dict], tgt_lab
 
     wanted_rois = []
     tmp_dir = "./temp"
+
+    print(f"{ocr_model} | {image} | {tgt_label}")
 
     try:
         # Start from 1: Skip class item
@@ -183,6 +189,7 @@ def detect_with_ocr(ocr_model: PaddleOCR, image, coord_list: list[dict], tgt_lab
                 text_found = True
 
             # 只记录包含目标文本的ROI
+            print(text_found)
             if text_found:
                 roi_info = {
                     "id": i,
@@ -267,8 +274,85 @@ def detect_medicine(model_dict, target_label: str, max_attempts=MAX_ATTEMPTS) ->
     
     return coord_list
 
+
+
+
+
+def detect_medicine_redefined(detect_med_model: YOLO, ocr_model: PaddleOCR, target_label: str, max_attempts=MAX_ATTEMPTS) -> list[dict]:
+    print("Detecting medicine:", target_label, "\n")
+
+    snapshot_fname = './temp/photo.jpg'
+    coord_list = []  # Perhaps List of Dicts
+    attempted = 1
+    roi_list = []  # {id, coordinates (xmin, ymin, xmax, ymax), center, ocr_text}
+    coord = []
+    conf = []
+
+
+
+    while True:
+    #for i in range(attempted, max_attempts + 1):
+        # try:
+        # Step 1: Taking a snapshot
+        #print("[Attempt {}/{}] Start capturing in few seconds...".format(i, max_attempts))
+        #auto_capture_photo(snapshot_fname)
+        #image = cv2.imread(snapshot_fname)
+        img, dep_img = kinect.use_kinect()
+        image = img
+        # Step 2: See if ANY medicine exists
+        coord, conf = detect_medicine_exist(image, detect_med_model)
+
+        # Step 3: See if the specific medicine is found
+        #roi_list = detect_with_ocr(ocr_model, image, coord, target_label)
+
+        # Success to find specific medicine(s), exit the loop
+        for tgt in coord:
+            #print("Start draw")
+            (xmin, xmax, ymin, ymax)  = (tgt["x_min"],tgt["x_max"], tgt["y_min"], tgt["y_max"])
+            midx = int((xmax+xmin)/2)
+            midy = int((ymax+ymin)/2)
+            cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+            dis = kinect.dismap(xmin, ymin,xmax, ymax, dep_img)
+
+            cv2.putText(img,str(round(dis,2)) + "mm, " + str(midx) +","+ str(midy), (xmin, ymax+20),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,50,50))
+
+ #           coord_list.append({
+ #               "xmin": tgt["coordinates"][0],
+ #               "ymin": tgt["coordinates"][1],
+ #               "xmax": tgt["coordinates"][2],
+ #               "ymax": tgt["coordinates"][3],
+ #           })
+
+        if len(coord_list) > 0:
+            break
+
+
+        cv2.imshow('Transformed Color Image', img)
+
+        time.sleep(0.05)
+        # Press q key to stop
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    if len(coord_list) == 0:
+        print("\n❌ Failed 3 times. Please check:")
+        print("1. Ensure bottle is centered in frame")
+        print("2. Adjust camera focus")
+        print("3. Clean camera lens")
+        print("4. Improve lighting conditions")
+        print("\n🔴 System stopped: Maximum error count reached")
+    else:
+        print("Medicine found!\n")
+
+    return coord_list
+
+
+
+
 def main():
-    detect_medicine()
+    model = YOLO("siou150.pt")
+
+    detect_medicine_redefined(model, PaddleOCR(use_angle_cls=True, lang='en'),"ACE inhibitor")
 
 
 if __name__ == "__main__":
